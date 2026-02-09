466c2cb1191579cc2800535d0f07448c010b8a77
pigeonIlesh <Ilyanikulin0820@gmail.com>
2026-02-09 16:45:37 +0300

    Добавьте файлы проекта.

---
+++ metod/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metod
+{
+    internal class Program
+    {
+        static int AddNumber()
+        {
+            Console.WriteLine($"Введите первое число");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Введите второе число");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int sum = num1 + num2;
+            return sum;
+        }
+        static void CheckEven()
+        {
+            Console.WriteLine($"Введите число");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"Число четное");
+            }
+            else
+            {
+                Console.WriteLine($"Число нечетное");
+            }
+        }
+        static void CalculateDiscount()
+        {
+            Console.WriteLine($"Введите цену товара:");
+            double price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Введите процент скидки:");
+            double discount = Convert.ToDouble(Console.ReadLine());
+            double discountAmount = price * (discount / 100);
+            double finalprice = price - discountAmount;
+            Console.WriteLine($"Начальная цена: {price}");
+            Console.WriteLine($"Финальная цена: {finalprice}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сумма чисел:");
+            Console.WriteLine(AddNumber());
+            Console.WriteLine(AddNumber());
+            Console.WriteLine(AddNumber());
+            Console.WriteLine("Проверка четности:");
+            CheckEven();
+            CheckEven();
+            CheckEven();
+            Console.WriteLine("Расчет скидки");
+            CalculateDiscount();
+            CalculateDiscount();
+            CalculateDiscount();
+        }
+    }
+}
---
+++ metod1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metod1
+{
+    internal class Program
+    {
+        static int Sum()
+        {
+            Console.WriteLine($"Введите первое число");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Введите второе число");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int sum = num1 + num2;
+            return sum;
+        }
+        static int Vach()
+        {
+            Console.WriteLine($"Введите первое число");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Введите второе число");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int vach = num1 - num2;
+            return vach;
+        }
+        static int Ym()
+        {
+            Console.WriteLine($"Введите первое число");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Введите второе число");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int ym = num1 * num2;
+            return ym;
+        }
+        static int Del()
+        {
+            Console.WriteLine($"Введите первое число");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Введите второе число");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int del = num1 / num2;
+            return del;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Калькулятор");
+            Console.WriteLine("Выбирете действие (+, -, *, /)");
+            string vvod = Console.ReadLine();
+            switch (vvod)
+            {
+                case "+":
+                    Console.WriteLine(Sum());
+                    break;
+                case "-":
+                    Console.WriteLine(Vach());
+                    break;
+                case "*":
+                    Console.WriteLine(Ym());
+                    break;
+                case "/":
+                    Console.WriteLine(Del());
+                    break;
+                default:
+                    Console.WriteLine($"Неверный ввод");
+                    break;
+            }
+        }
+    }
+}
